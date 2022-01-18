f442c05f410cf11a6cc09ccc9cd5192ed4111c59
Richard Teammco <teammcr192@gmail.com>
2022-01-18 00:48:10 -0500

    Updated particles for rain to be eliptical and more rain-like

--- src/particles/RainParticle.ts
+++ src/particles/RainParticle.ts
@@ -8,7 +8,7 @@ export default class RainParticle implements Particle {
 
   constructor() {
     this.speed = 0.001 + 0.001 * Math.random();
-    this.dropletSize = 0.5 + 3 * Math.random();
+    this.dropletSize = 1 + 5 * Math.random();
     this.xPos = Math.random();
     this.yPos = 0;
   }
@@ -25,8 +25,8 @@ export default class RainParticle implements Particle {
     context.beginPath();
     const pixelX = this.xPos * canvas.width;
     const pixelY = this.yPos * canvas.height;
-    context.arc(pixelX, pixelY, this.dropletSize, 0, Math.PI * 2);
-    context.fillStyle = 'rgba(0, 0, 255, 0.4)';
+    context.ellipse(pixelX, pixelY, this.dropletSize / 4, this.dropletSize, 0, 0, Math.PI * 2);
+    context.fillStyle = 'rgba(0, 40, 255, 0.4)';
     context.fill();
   }
 }
