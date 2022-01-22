34952b96634f46b74bbd003bc273aa96a9743fb8
Richard Teammco <teammcr192@gmail.com>
2022-01-21 21:24:48 -0500

    Installed and tested the canvas-pixel library in the React app.

--- test/react-app/src/App.tsx
+++ test/react-app/src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import * as X from 'canvas-pixel';
-const cp = require('canvas-pixel');
+import { start, stop, clear } from 'canvas-pixel';
 
 function App() {
+  useEffect(() => {
+    start();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
